356b4cc2da39f4aeff91fc18bbb9386bf1f995ff
纪承琼 <597684882@qq.com>
2019-12-20 15:16:56 +0800

    no message

--- linux.sh
+++ linux.sh
@@ -1,2 +1,122 @@
 #!/bin/bash
+# 查看共享内存
+# ipcs -m
+
+# 查看进程
+# ps ax
+
+# terminfo 数据库
+# infocmp vt100
+# echo $TERM
+
+# 查看bash shell信息
+# man bash
+
+# ls查看文件
+# ls 
+# ls -F
+# ls -a
+# ls -F -R ..
+# ls -l
+# ls -sail
+# ls -l linux.sh
+# ls -l linu?.sh
+
+# 创建文件
+# touch test1
+# ls -l test1
+# touch -t 200812251200 test1
+# ls -l test1
+
+# 复制文件
+# cp -L test1 test4
+# cp -s test1 test5
+# ln -s test1 test5
+# cp -l test1 test2
+
+# 重命名文件
+# mv test1 test2
+# ls -il test*
+
+# 删除文件
+rm -i test2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
